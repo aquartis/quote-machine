52a7a04d5c29f18d5eec2922c99ec2f21cfc23d5
Eugene Worker <1zheka92@gmail.com>
2019-07-29 12:18:13 +0300

    Updates

---
+++ precache-manifest.a6ba2fea9c0146067628d8a99d52a956.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27aaf361cce647327c47a80ebb2e4e2e",
+    "url": "/quote-machine/index.html"
+  },
+  {
+    "revision": "17c01f14527d554aeb5e",
+    "url": "/quote-machine/static/css/main.d0f88754.chunk.css"
+  },
+  {
+    "revision": "ec5b35233fd5e26b47f3",
+    "url": "/quote-machine/static/js/2.4c6cd1f5.chunk.js"
+  },
+  {
+    "revision": "17c01f14527d554aeb5e",
+    "url": "/quote-machine/static/js/main.a16d196d.chunk.js"
+  },
+  {
+    "revision": "bc2b7c61d93870784a32",
+    "url": "/quote-machine/static/js/runtime~main.85834c65.js"
+  }
+]);
